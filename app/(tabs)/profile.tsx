0dcf425038e381ac2d0ccbcf5ed22acccafe15f5
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-01-24 20:22:10 +0300

    add login form and profile logic

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,30 +1,20 @@
-// import { useAuth } from "@clerk/clerk-expo"
-// import { Link } from "expo-router"
-// import { Button, H2, Text, View, YStack } from "tamagui"
-
-// export default function Profile() {
-//   const { signOut, isSignedIn } = useAuth()
-
-//   return (
-//     <YStack gap="$2">
-//       {isSignedIn ? (
-//         <YStack>
-//           <H2>You are signed in 😄</H2>
-//           <Button onPress={() => signOut()}>Log out</Button>
-//         </YStack>
-//       ) : (
-//         <Link href={"/(modals)/login"}>Log in</Link>
-//       )}
-//     </YStack>
-//   )
-// }
-
-import { Text, View } from "tamagui"
+import { useAuth } from "@clerk/clerk-expo"
+import { Link } from "expo-router"
+import { Button, H2, YStack } from "tamagui"
 
 export default function Profile() {
+  const { signOut, isSignedIn } = useAuth()
+
   return (
-    <View flex={1} alignItems="center">
-      <Text fontSize={20}>Profile</Text>
-    </View>
+    <YStack gap="$2">
+      {isSignedIn ? (
+        <YStack>
+          <H2>You are signed in 😄</H2>
+          <Button onPress={() => signOut()}>Log out</Button>
+        </YStack>
+      ) : (
+        <Link href={"/(modals)/login"}>Log in</Link>
+      )}
+    </YStack>
   )
 }
